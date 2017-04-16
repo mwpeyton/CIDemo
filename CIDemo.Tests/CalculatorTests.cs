98565d7e6c6dcbfea791046ed6ad66febf5bef43
Mike <mikewpeyton@gmail.com>
2017-04-15 20:04:11 -0500

    test commit nunit

---
+++ CIDemo.Tests/CalculatorTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+
+namespace CIDemo.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+
+        //Calculator _calculator;
+
+        //[OneTimeSetUp]
+        //public void TestSetup()
+        //{
+        //    //Assemble
+        //    _calculator = new Calculator();
+        //}
+
+        //[Test]
+        //public void ShouldAddTwoNumbers()
+        //{
+        //    //Expected 
+        //    var expectedResult = 15;
+
+        //    //Actual
+        //    var actualResult = _calculator.Add(7, 8);
+
+        //    Assert.That(expectedResult, Is.EqualTo(actualResult));
+        //}
+
+        //[Test]
+        //public void ShouldMulTwoNumbers()
+        //{
+        //    //Expected 
+        //    var expectedResult = 56;
+
+        //    //Actual
+        //    var actualResult = _calculator.Mul(7, 8);
+
+        //    Assert.That(expectedResult, Is.EqualTo(actualResult));
+        //}
+
+        //[OneTimeTearDown]
+        //public void TestTearDown()
+        //{
+        //    //disassemble
+        //    _calculator = null;
+        //}
+    }
+}
